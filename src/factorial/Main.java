e7530848bf00c3ba48c2af86fca8e532c9344e11
Stanislav Yotov <yotov.st@gmail.com>
2016-03-06 19:42:51 +0000

    Main

---
+++ src/factorial/Main.java
@@ -0,0 +1,10 @@
+package factorial;
+
+public class Main {
+	public static void main(String[] args) {
+		Factorial fact = new Factorial();
+		for (int i = 1; i <= 21; i++) {
+			System.out.println(fact.calculate(i));
+		}
+	}
+}
